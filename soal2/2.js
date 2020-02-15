f060c832adcb5d64842ebda7ee12b7c4c2c73a44
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 21:05:55 +0700

    perbaikan username validasi

--- soal2/2.js
+++ soal2/2.js
@@ -1,1 +1,3 @@
-const is_username_valid = (username) =>  /^[a-z]{5,9}$/test(username);
+const is_username_valid = (username) =>  /^[a-z]{5,9}$/.test(username);
+
+console.log(is_username_valid('jok3r'));
