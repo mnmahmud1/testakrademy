525c887dbdbffc07a8a912726f56375bf8e1bd2f
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 10:21:27 +0700

    mendefinisikan var hasil (2)

--- soal3/3.php
+++ soal3/3.php
@@ -3,7 +3,7 @@
         $pecah = str_split($kata); //memecah kata menjadi array
         $hit = count($pecah); //hitung isi setelah dipecah
         $hasil = null; //mendefinisikan variable hasil
-        for ($i=0; $i < $hit ; $i++) { 
+        for ($i= 0; $i < $hit ; $i++) { 
             if ($huruf == $pecah[$i]){
                 $hasil += 1;
             }
