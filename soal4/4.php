cd8d202a110b8143614299115eb3aee20fe62e2b
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 10:57:06 +0700

    melakukan penambahan baris untuk memulai pengecekan dari kode hex

--- soal4/4.php
+++ soal4/4.php
@@ -4,14 +4,24 @@
         $hit = count($pecah); //menghitung hasil pecah
         $pagar = array();
         $kode = array();
-        $i = 0;
+        $i = 0; //mendefinisikan var $i untuk looping
         while ($i < $hit) { 
             if ($pecah[$i] == '#'){
                 $pecah[$i] = $pagar[$i];
             } else {
-                $pecah[$i-1] = $pagar[$i -1];
+                $pecah[$i-1] = $kode[$i -1];
             }
             $i++;
+        }
+
+        $hitpagar = count($pagar);
+        $hitkode = count($kode);
+        $j = 0; //mendefinisikan var $j untuk looping
+
+        if (($hitpagar == 1)&&($hitkode == 3 || $hitkode ==6 )) {
+            # code...
+        } else {
+            echo "Format Hex Code salah!";
         }
 
     }
