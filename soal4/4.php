935f4c3c023b5cca26208bda5cf4fac1e2d3dde7
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 10:45:55 +0700

    membuat peletakan kode hex ke masing -  masing array tersendiri

--- soal4/4.php
+++ soal4/4.php
@@ -1,11 +1,17 @@
 <?php
     function validateColor($hex){
         $pecah = str_split($hex); //memecah kode hex
-        $hit = count($pecah);
+        $hit = count($pecah); //menghitung hasil pecah
         $pagar = array();
         $kode = array();
-        for ($i=0; $i < $hit ; $i++) { 
-            # code...
+        $i = 0;
+        while ($i < $hit) { 
+            if ($pecah[$i] == '#'){
+                $pecah[$i] = $pagar[$i];
+            } else {
+                $pecah[$i-1] = $pagar[$i -1];
+            }
+            $i++;
         }
 
     }
