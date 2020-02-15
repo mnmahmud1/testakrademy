e1dd9f98efc246698518bcc44698c43992e010fe
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 10:12:49 +0700

    mendefinisikan variable hasil

--- soal3/3.php
+++ soal3/3.php
@@ -2,6 +2,7 @@
     function countChar($kata, $huruf) {
         $pecah = str_split($kata); //memecah kata menjadi array
         $hit = count($pecah); //hitung isi setelah dipecah
+        $hasil = null; //mendefinisikan variable hasil
         for ($i=0; $i < $hit ; $i++) { 
             if ($huruf == $pecah[$i]){
                 $hasil += 1;
