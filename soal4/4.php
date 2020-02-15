ada83204693c71b0d3e729c83ed6bff191e41c17
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 12:30:45 +0700

    pembersihan comment yang tidak di perlukan dan pemberian input function sesuai seperti soal

--- soal4/4.php
+++ soal4/4.php
@@ -11,19 +11,13 @@
                 $kode[$i - 1] = $pecah[$i];
             }
         }
-        // echo $pagar[0];
-        // echo $kode[0];
-        // echo $kode[1];
-        // echo $kode[2];
-        //$hexcode = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F');
+        
         $hitpagar = count($pagar);
         $hitkode = count($kode);
-        //$hithexcode = count($hexcode);
         $betul = array();
         $hasil = null;
         if (($hitpagar == 1)&&($hitkode == 3 || $hitkode == 6 )) {
-            for ($j=0; $j < $hitkode ; $j++) { 
-                //for ($k=0; $k < $hithexcode ; $k++) { 
+            for ($j=0; $j < $hitkode ; $j++) {  
                     if ($kode[$j] == 1) {
                         $hasil = 1;
                     } elseif($kode[$j] == 2) {
@@ -69,30 +63,32 @@
                     } else {
                         $hasil = 0;
                     }
-                //}
                 $betul[$j] = $hasil;
             }
         } else {
-            echo "Format Hex Code salah!(1)";
+            echo "Format Hex Code salah!";
             exit;
         }
 
         $hitung = null;
         $hitbetul = count($betul);
         for ($l=0; $l < $hitbetul ; $l++) { 
-            //echo $betul[$l];
             if ($betul[$l] == 1) {
                 $hitung += 1;
             } else {
-                echo "Format Hex Code salah!(2)";
+                echo "Format Hex Code salah!";
                 exit;
             }
         }
         if ($hitung == 3 || $hitung == 6) {
             echo "Format Hex Code benar!";
         } else {
-            echo "Format Hex Code salah!(3)";
+            echo "Format Hex Code salah!";
         }
     }
     validateColor("#eee");
+    echo "<br>";
+    validateColor("#F3F3F3");
+    echo "<br>";
+    validateColor("#ezff8d");
 ?>
