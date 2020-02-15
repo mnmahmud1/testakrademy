cb07cdaecc77d72c46f58196e58d75f85d78e735
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 20:46:10 +0700

    pembuatan regex dasar untuk memvalidasi username

--- soal2/2.php
+++ soal2/2.php
@@ -1,7 +1,10 @@
 <?php
-function is_username_valid ($name){
-
-    
+function is_username_valid($name){
+    if (preg_match("[a-z]",$name)) {
+        echo "String betul";
+    } else {
+        echo "salah";
+    }
 }
-
+    is_username_valid("mahmudi");
 ?>
