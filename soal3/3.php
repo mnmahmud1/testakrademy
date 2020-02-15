56ac8e91a3a25f52facd1c02edd4b46b9587400c
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 10:02:54 +0700

    membuat looping dan percabangan untuk menghitung jumlah huruf

--- soal3/3.php
+++ soal3/3.php
@@ -1,9 +1,12 @@
 <?php
     function countChar($kata, $huruf) {
-        $pecah = str_split($kata); //memecah kata
+        $pecah = str_split($kata); //memecah kata menjadi array
         $hit = count($pecah); //hitung isi setelah dipecah
-        
-
+        for ($i=0; $i < $hit ; $i++) { 
+            if ($huruf == $pecah[$i]){
+                $hasil += 1;
+            }
+        }
 
     }
 
