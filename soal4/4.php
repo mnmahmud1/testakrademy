60dab19d6d13197ce625df5bb23158f26216832b
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 11:40:13 +0700

    perbaikan

--- soal4/4.php
+++ soal4/4.php
@@ -6,9 +6,9 @@
         $kode = array();
         for ($i = 0; $i < $hit ; $i++) { 
             if ($pecah[$i] == '#'){
-                $pecah[$i] = $pagar[$i];
+                $pagar[$i] = $pecah[$i];
             } else {
-                $pecah[$i-1] = $kode[$i -1];
+                $kode[$i -1] = $pecah[$i-1];
             }
         }
         $hexcode = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "A", "B", "C", "D", "E", "F");
