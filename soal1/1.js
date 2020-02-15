7e1d76165df413a274963502a3894d6c383499a2
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 19:35:36 +0700

    pembuatan return biodata data diri

---
+++ soal1/1.js
@@ -0,0 +1,24 @@
+const myBiodata = () => {
+    return {
+        'name' : 'Muhammad Nurhasan Mahmudi',
+        'age'  : 19,
+        'address' : 'Bogor',
+        'hobbies' : ['Reading', 'Programming', 'Traveling', 'game', 'computering'],
+        'is_married' : false, 
+        'list_school' : [
+            
+        ]
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
