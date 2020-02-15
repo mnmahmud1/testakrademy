e8deeba53db4735f20818781384b109cecf3b407
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 11:33:27 +0700

    membuat pengecekan dan perhitungan benar dan tidak benarnya hex code

--- soal4/4.php
+++ soal4/4.php
@@ -13,16 +13,42 @@
             }
             $i++;
         }
-
+        $hexcode = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "A", "B", "C", "D", "E", "F");
         $hitpagar = count($pagar);
         $hitkode = count($kode);
-        $j = 0; //mendefinisikan var $j untuk looping
-
-        if (($hitpagar == 1)&&($hitkode == 3 || $hitkode ==6 )) {
-            # code...
+        $hithexcode = count($hexcode);
+        $betul = array();
+        $hasil = null;
+        if (($hitpagar == 1)&&($hitkode == 3 || $hitkode == 6 )) {
+            for ($j=0; $j < $hitkode ; $j++) { 
+                for ($k=0; $k < $hithexcode ; $k++) { 
+                    if ($kode[$j] == $hexcode[$k]) {
+                        $hasil = 1;
+                    } else {
+                        $hasil = 0;
+                    }
+                }
+                $betul[$j] = $hasil;
+            }
+        } else {
+            echo "Format Hex Code salah!";
+            end;
+        }
+        $hitung = null;
+        $hitbetul = count($betul);
+        for ($l=0; $l < $hitbetul ; $l++) { 
+            if ($betul[$l] == 1) {
+                $hitung += 1;
+            } else {
+                echo "Format Hex Code salah!";
+                end;
+            }
+        }
+        if ($hitung == 3 || $hitung == 6) {
+            echo "Format Hex Code benar!";
         } else {
             echo "Format Hex Code salah!";
         }
-
     }
+    validateColor("#eee");
 ?>
