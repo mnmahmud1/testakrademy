55e2c3fe648445f0a05c4e0820d8420f4115ab06
mnmahmud1 <mahmudhagano01@gmail.com>
2020-02-15 21:14:18 +0700

    perbaikan console log

--- soal2/2.js
+++ soal2/2.js
@@ -2,4 +2,4 @@ const is_username_valid = (username) =>  /^[a-z]{5,9}$/.test(username);
 console.log(is_username_valid('jok3r'));
 
 const is_password_valid = (password) =>  /(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[\W|_])(?=.*[=]).{8,}$/.test(password);
-is_password_valid('Dap0#r');
+console.log(is_password_valid('Dap0#r'));
